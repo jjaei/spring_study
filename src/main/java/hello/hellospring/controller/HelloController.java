9b5698427c113f494c9e9da43d12f6c7687ed335
jjaei <ieajnowk@gmail.com>
2023-04-19 10:56:21 +0900

    회원 도메인과 리포지토리 만들기

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,10 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,40 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+        // 웹 페이지에서페이지 원본보기(소스) 하면 html 소스 없이 "hello spring"만 달랑 보임.
+    }
+
+    // api 방식 -> {"name" : "spring!!"} 형식으로 출력됨.
+    // JSON 방식 -> 키, 밸류로 이루어진 구조 (심플)
+    @GetMapping("hello-api")
+    @ResponseBody  // JSON으로 반환하는 게 디폴트로 되어있음.
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,23 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;  // 임의의 값(시스템이 저장하는 id)
+    private String name;  // 이름
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
