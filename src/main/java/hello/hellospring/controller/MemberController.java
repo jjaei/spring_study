a948816d358ca7aadbd6d4dac40729a4dfd24d77
jjaei <ieajnowk@gmail.com>
2023-05-15 08:53:58 +0900

    config 파일 작성을 위해 애노테이션 제거

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
     private final MemberService memberService;
-
+    //new로 객체를 생성해서 쓰면 MemberController 뿐만 아니라 다른 컨트롤러도 이 new 객체를 가져다 쓸 수 있다.
+    // 하나만 생성해서 같이 공유해서 쓰기로 한다. (생성자, Autowired)
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
