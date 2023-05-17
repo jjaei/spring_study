a575d4d12f7ca07f01760a93d018105d5b6c0e39
jjaei <ieajnowk@gmail.com>
2023-05-17 15:10:02 +0900

    스프링 설정 변경

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean  // 스프링 빈 등록으로 이해 -> 등록
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
