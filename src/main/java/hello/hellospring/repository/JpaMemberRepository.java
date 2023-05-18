2db414988c5e737c7ed744c5fed65e4367006a2b
jjaei <ieajnowk@gmail.com>
2023-05-18 09:39:28 +0900

    유니코드 수정

--- src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -11,18 +11,22 @@ public class JpaMemberRepository implements MemberRepository {
     public JpaMemberRepository(EntityManager em) {
         this.em = em;
     }
+
     public Member save(Member member) {
         em.persist(member);
         return member;
     }
+
     public Optional<Member> findById(Long id) {
         Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
     }
+
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
+
     public Optional<Member> findByName(String name) {
         List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", name)
