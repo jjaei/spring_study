9601c11a595c83af327ed4f8bc066de84edfd6c8
jjaei <ieajnowk@gmail.com>
2023-05-23 10:07:27 +0900

    문장 수정

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -3,11 +3,9 @@ package hello.hellospring.domain;
 import jakarta.persistence.*;
 
 @Entity
-
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // 임의의 값(시스템이 저장하는 id)
     private String name;  // 이름
 
