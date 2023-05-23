7d409c404e1c5adfcdb53ed164c34f39f8581086
jjaei <ieajnowk@gmail.com>
2023-05-24 08:52:40 +0900

    AOP 생성으로 기존에 있던 공통 코드 삭제

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,27 +14,17 @@ import java.util.Optional;
 public class MemberService {
     private final MemberRepository memberRepository;
 
-
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
 
     public Long join(Member member) {
         // 회원 가입
         // 같은 이름의 중복 회원은 허용하지 않는다는 룰 생성
 
-        long start = System.currentTimeMillis();
-
-        try {
-            validateDuplicateMember(member);  // 중복 회원 검증 메서드
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member);  // 중복 회원 검증 메서드
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -45,16 +35,8 @@ public class MemberService {
     }
 
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-
-        try {
             // 전체 회원 조회
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
