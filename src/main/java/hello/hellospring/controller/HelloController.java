6fb58da343027ff3e6c164ea3ef166a0ad037de6
jjaei <ieajnowk@gmail.com>
2023-07-27 14:57:34 +0900

    HelloController

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "hello!!");
+        model.addAttribute("data", "spring!!");
         return "hello";
     }
 }
