f3d51eb0e2081c2208d38d4e68cce49ee1c93040
jjaei <ieajnowk@gmail.com>
2023-07-27 14:29:16 +0900

    setting

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,8 +2,9 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
