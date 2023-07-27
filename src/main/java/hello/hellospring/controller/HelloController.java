e88f228652e63604cd5747f92339fdab5e762662
jjaei <ieajnowk@gmail.com>
2023-07-27 15:47:48 +0900

    @ResponseBody 객체 반환

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,32 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody  // http의 body에 데이터를 직접 넣겠다는 애노테이션
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;  // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody  // 객체로 받는다면 json 형식으로 반환
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
