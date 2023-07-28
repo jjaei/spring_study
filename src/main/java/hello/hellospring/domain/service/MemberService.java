e82312e570fce1b17d4690984b656640b89618fa
jjaei <ieajnowk@gmail.com>
2023-07-28 16:42:42 +0900

    회원 서비스 개발

---
+++ src/main/java/hello/hellospring/domain/service/MemberService.java
@@ -0,0 +1,36 @@
+package hello.hellospring.domain.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 중복 회원은 가입 x
+        validateDuplicateMember(member);  // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{ // null이 아니고 값이 있으면
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
