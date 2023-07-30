f0143c04085856077488ac55488cecf1d2cd3cae
jjaei <ieajnowk@gmail.com>
2023-07-30 16:34:24 +0900

    JPA로 Entity 매핑

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
 
     public Long getId() {
