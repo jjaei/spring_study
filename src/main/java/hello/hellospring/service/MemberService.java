55e9624e506ae7c987ed211a68ba388e9c4cdb5c
jjaei <ieajnowk@gmail.com>
2023-07-31 14:42:44 +0900

    공통관심사항 분리 -> 시간 측정 로직 분리

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -18,17 +18,9 @@ public class MemberService {
     // 회원 가입
     public Long join(Member member) {
         // 중복 회원은 가입 x
-
-        long start = System.currentTimeMillis();
-        try {
             validateDuplicateMember(member);  // 중복 회원 검증
             memberRepository.save(member);
             return member.getId();
-        }finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join : " + timeMs + "ms");
-        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -40,14 +32,7 @@ public class MemberService {
 
     // 전체 회원 조회
     public List<Member> findMember() {
-        long start = System.currentTimeMillis();
-        try {
             return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish-start;
-            System.out.println("findMember : " + timeMs + "ms");
-        }
     }
 
     public Optional<Member> findOne(Long memberId){
