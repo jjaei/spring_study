18605d7968f5fad227f060fb8f16cd03c5b8b33a
jjaei <ieajnowk@gmail.com>
2023-08-04 18:27:14 +0900

    빈 설정 메타정보 확인

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class BeanDefinitionTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig .class);
+//    GenericXmlApplicationContext ac2 = new GenericXmlApplicationContext("appConfig.xml");
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
