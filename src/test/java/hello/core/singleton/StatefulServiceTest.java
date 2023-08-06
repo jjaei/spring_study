c35072c6ae00d569cabe65f84523aea2bf7c68c3
jjaei <ieajnowk@gmail.com>
2023-08-06 16:28:22 +0900

    상태를 유지할 경우 발생하는 문제점

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,37 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A 사용자가 10000원 주문
+        statefulService1.order("userA", 10000);
+
+        // ThreadB : B 사용자가 20000원 주문
+        statefulService1.order("userB", 20000);
+
+        // ThreadA : 사용자 A가 주문 금액을 조회하면...?
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
