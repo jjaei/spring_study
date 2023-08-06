766b0bcd5b29e843cfef7e4cb4fbce80876f9023
jjaei <ieajnowk@gmail.com>
2023-08-06 16:43:43 +0900

    무상태로 수정

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,14 +2,15 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price; // 상태를 유지하는 필드 10000 -> 20000
+//    private int price; // 상태를 유지하는 필드 10000 -> 20000
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price;  // 이 부분이 문제!
-    }
-
-    public int getPrice() {
+//        this.price = price;
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class StatefulServiceTest {
 
     @Test
@@ -15,16 +13,16 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // ThreadA : A 사용자가 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         // ThreadB : B 사용자가 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         // ThreadA : 사용자 A가 주문 금액을 조회하면...?
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
