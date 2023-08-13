c5a14e289c1f3dc474fe75d583002b284fbfe543
jjaei <ieajnowk@gmail.com>
2023-08-13 16:25:02 +0900

    원래대로 돌리기

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,6 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +14,7 @@ public class OrderServiceImpl implements OrderService{
 /*    @Autowired  생성자가 하나일 경우 Autowired 생략 가능
       @RequiredArgsConstructor가 있어서 생성자를 작성해주지 않아도 됨.
 */
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
